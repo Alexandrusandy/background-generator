ea835b993dac695f0fe4a7d76605ef3b51c1b0a1
Alexandrusandy <68395084+Alexandrusandy@users.noreply.github.com>
2020-08-02 16:30:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var random = document.querySelector('button');
+
+function setGradient(){
+	body.style.background =
+	"linear-gradient(to right, " +color1.value
+	+"," + color2.value+")";
+
+	css.textContent = body.style.background + ";";
+}
+
+
+color1.addEventListener("input", setGradient);
+	
+color2.addEventListener("input", setGradient);
+
+
+randomColor = () => {
+	let letters = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+};
+
+
+random.addEventListener('click', () => {
+	color1.value = randomColor();
+	color2.value = randomColor();
+	setGradient();
+});
